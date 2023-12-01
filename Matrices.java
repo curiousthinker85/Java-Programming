e8ffceb04aca92c1e5c919a6f79dd882d4b8c95e
curiousthinker85 <adityaasabe852002@gmail.com>
2023-12-01 16:13:56 +0530

    diagonal sum

--- Matrices.java
+++ Matrices.java
@@ -56,6 +56,31 @@ public class Matrices {
         
     }
 
+    public static int diagonalSum(int matrix[][]){
+        int sum = 0;
+
+        // for(int i = 0; i<matrix.length; i++){
+        //     for(int j = 0; j< matrix.length; j++){
+        //         if(i == j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        // return sum;
+
+        for(int i = 0; i<matrix.length; i++){
+            sum += matrix[i][i];
+
+            if(i != matrix.length-1-i){
+                sum += matrix[i][matrix.length-i-1];
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         // int matrix[][] = new int[3][3];
         // int n = matrix.length, m = matrix[0].length;
@@ -82,6 +107,7 @@ public class Matrices {
                         {9,10,11,12},
                         {13,14,15,16}};
 
-        printSpiral(matrix);
+        // printSpiral(matrix);
+        System.out.println(diagonalSum(matrix));
     }
 }
